f66f02db38ccacc6545106a24689a50967f9dd6c
deadcheat <deadcheat@users.noreply.github.com>
2018-06-22 02:18:25 +0900

    added handler

---
+++ handler.go
@@ -0,0 +1,63 @@
+package alexa
+
+// LambdaHandler request handler works with github.com/aws/aws-lambda-go
+type LambdaHandler interface {
+	Handle(RequestEnvelope) (ResponseEnvelope, error)
+	HandleIntent(intentType string, h Handler)
+	HandleLaunch(Handler)
+	HandleEnd(Handler)
+}
+
+// Handler intent handler func
+type Handler func(RequestEnvelope) (ResponseEnvelope, error)
+
+// DefaultLambdaHandler default implement for LambdaHandler
+type DefaultLambdaHandler struct {
+	intentHandlers map[string]Handler
+	launchHandler  Handler
+	endHandler     Handler
+}
+
+// NewLambdaHandler return new lambda handler implement
+func NewLambdaHandler() LambdaHandler {
+	return &DefaultLambdaHandler{
+		intentHandlers: make(map[string]Handler),
+		launchHandler:  nil,
+		endHandler:     nil,
+	}
+}
+
+// Handle handle all lambda requests
+func (d *DefaultLambdaHandler) Handle(req RequestEnvelope) (ResponseEnvelope, error) {
+	if d.launchHandler == nil || d.endHandler == nil {
+		return EmptyResponse, ErrNecessaryHandlersAreNotAssigned
+	}
+	switch req.Request.Type {
+	case TypeLaunchRequest:
+		return d.launchHandler(req)
+	case TypeSessionEndedRequest:
+		return d.endHandler(req)
+	case TypeIntentRequest:
+		h, ok := d.intentHandlers[req.Intent.Name]
+		if ok {
+			return h(req)
+		}
+		return EmptyResponse, ErrNoIntentToHandle
+	}
+	return EmptyResponse, ErrNoHandler
+}
+
+// HandleIntent put intent handler to internal map
+func (d *DefaultLambdaHandler) HandleIntent(intentType string, h Handler) {
+	d.intentHandlers[intentType] = h
+}
+
+// HandleLaunch put request launch handler to internal map
+func (d *DefaultLambdaHandler) HandleLaunch(h Handler) {
+	d.launchHandler = h
+}
+
+// HandleEnd put request sessionended handler to internal map
+func (d *DefaultLambdaHandler) HandleEnd(h Handler) {
+	d.endHandler = h
+}
---
+++ values.go
@@ -0,0 +1,71 @@
+package alexa
+
+import "errors"
+
+// const value of request.type
+const (
+	TypeLaunchRequest       = "LaunchRequest"
+	TypeIntentRequest       = "IntentRequest"
+	TypeSessionEndedRequest = "SessionEndedRequest"
+)
+
+// const value of request.intent.name
+const (
+	IntentAMAZONCancelIntent     = "AMAZON.CancelIntent"
+	IntentAMAZONHelpIntent       = "AMAZON.HelpIntent"
+	IntentAMAZONLoopOffIntent    = "AMAZON.LoopOffIntent"
+	IntentAMAZONLoopOnIntent     = "AMAZON.LoopOnIntent"
+	IntentAMAZONNextIntent       = "AMAZON.NextIntent"
+	IntentAMAZONNoIntent         = "AMAZON.NoIntent"
+	IntentAMAZONPauseIntent      = "AMAZON.PauseIntent"
+	IntentAMAZONPreviousIntent   = "AMAZON.PreviousIntent"
+	IntentAMAZONRepeatIntent     = "AMAZON.RepeatIntent"
+	IntentAMAZONResumeIntent     = "AMAZON.ResumeIntent"
+	IntentAMAZONShuffleOffIntent = "AMAZON.ShuffleOffIntent"
+	IntentAMAZONShuffleOnIntent  = "AMAZON.ShuffleOnIntent"
+	IntentAMAZONStartOverIntent  = "AMAZON.StartOverIntent"
+	IntentAMAZONStopIntent       = "AMAZON.StopIntent"
+	IntentAMAZONYesIntent        = "AMAZON.YesIntent"
+)
+
+// OutputSpeech type
+const (
+	TypePlainText = "PlainText"
+	TypeSSML      = "SSML"
+)
+
+// Slot type
+const (
+	SlotAMAZONDate            = "AMAZON.DATE"
+	SlotAMAZONDuration        = "AMAZON.DURATION"
+	SlotAMAZONFourDigitNumber = "AMAZON.FOUR_DIGIT_NUMBER"
+	SlotAMAZONNumber          = "AMAZON.NUMBER"
+	SlotAMAZONPhoneNumber     = "AMAZON.PhoneNumber"
+	SlotAMAZONTime            = "AMAZON.TIME"
+	SlotAMAZONSearchQuery     = "AMAZON.SearchQuery"
+	SlotAMAZONActor           = "AMAZON.Actor"
+	SlotAMAZONAnimal          = "AMAZON.Animal"
+	SlotAMAZONArtist          = "AMAZON.Artist"
+	SlotAMAZONAuthor          = "AMAZON.Author"
+	SlotAMAZONBook            = "AMAZON.Book"
+	SlotAMAZONCity            = "AMAZON.City"
+	SlotAMAZONColor           = "AMAZON.Color"
+	SlotAMAZONCorporation     = "AMAZON.Corporation"
+	SlotAMAZONCountry         = "AMAZON.Country"
+	SlotAMAZONFirstName       = "AMAZON.FirstName"
+	SlotAMAZONFood            = "AMAZON.Food"
+	SlotAMAZONLanguage        = "AMAZON.Language"
+	SlotAMAZONMovie           = "AMAZON.Movie"
+	SlotAMAZONPerson          = "AMAZON.Person"
+	SlotAMAZONRegion          = "AMAZON.Region"
+	SlotAMAZONRoom            = "AMAZON.Room"
+)
+
+// variables
+var (
+	EmptyResponse = ResponseEnvelope{}
+
+	ErrNoHandler                       = errors.New("theare are no handlers assigned with specified request")
+	ErrNoIntentToHandle                = errors.New("specified intent was not found")
+	ErrNecessaryHandlersAreNotAssigned = errors.New("handlers for launch request and session end request are both needed")
+)
